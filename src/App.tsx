1c9aee17014c8d3104deeeb5eb9e5a0fbda99d41
Sajitha Saranga <109761136+saranga1998@users.noreply.github.com>
2024-09-28 12:41:05 +0530

    Advanced Props

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,41 @@
 
 import './App.css'
+import { Heading } from './Components/Advanced/Heading'
+import { Oscar } from './Components/Advanced/Oscar'
+import { Status } from './Components/Advanced/Status'
 import { Greet } from './Components/Greet'
-import { Person } from './Components/Person'
-import { PersonList } from './Components/PersonList'
+// import { Person } from './Components/Person'
+// import { PersonList } from './Components/PersonList'
 
 function App() {
   
-  const personName ={
-    fname :'Sajitha',
-    lname :'Saranga',
-  }
+  // const personName ={
+  //   fname :'Sajitha',
+  //   lname :'Saranga',
+  // }
 
-  const personList = [
-    {
-      fname : 'Anura',
-      lname : 'Kumara',
-    },
-    {
-      fname : 'Namal',
-      lname : 'Rajapaksha',
-    }
-  ]
+  // const personList = [
+  //   {
+  //     fname : 'Anura',
+  //     lname : 'Kumara',
+  //   },
+  //   {
+  //     fname : 'Namal',
+  //     lname : 'Rajapaksha',
+  //   }
+  // ]
 
   return (
     <>
-      <Greet name= 'Sajitha' messageCount={20} isLoggedIn={true}/>
+      {/* <Greet name= 'Sajitha' messageCount={20} isLoggedIn={true}/>
       <Person name={personName}/>
-      <PersonList names={personList}/>
+      <PersonList names={personList}/> */}
+      <Status status='success'/>
+      <Heading>PlaceHolder Text</Heading>
+      <Oscar>
+        <Heading>Oscar Goes to Oliver</Heading>
+        <Greet name= 'Sajitha' isLoggedIn={true}/>
+      </Oscar>
     </>
   )
 }
